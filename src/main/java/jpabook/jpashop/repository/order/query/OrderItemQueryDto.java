e7337a303a3ba9dafd480dd118aea044b02c7ad7
gunners <chakagae123@naver.com>
2024-04-28 01:30:20 +0900

    * spring2 섹션 4. 주문 조회 V6: JPA에서 DTO 직접 조회, 플랫 데이터 최적화

--- src/main/java/jpabook/jpashop/repository/order/query/OrderItemQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderItemQueryDto.java
@@ -8,13 +8,13 @@ public class OrderItemQueryDto {
 
     @JsonIgnore //만약 필요없다면
     private Long orderId;
-    private String ItemName;
+    private String itemName;
     private int orderPrice;
     private int count;
 
     public OrderItemQueryDto(Long orderId, String itemName, int orderPrice, int count) {
         this.orderId = orderId;
-        ItemName = itemName;
+        this.itemName = itemName;
         this.orderPrice = orderPrice;
         this.count = count;
     }
